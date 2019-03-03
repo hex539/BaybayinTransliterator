7fa6a91f0d7fa8135b0a5b79ddce38d9f6bc6b72
Robin Lee <rglee32@gmail.com>
2019-03-03 17:49:30 +0100

    Add CSS and declare a place for the font to live.

---
+++ service.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import app.app
+
+from bottle import request
+from bottle import route
+from bottle import run
+from bottle import static_file
+
+import os.path
+
+@route('/')
+def index():
+    return static_file('index.html', root='static_files')
+
+
+@route('/', method='post')
+def index_post():
+    return ''.join(map(str, app.app.transform(request.forms.get('input'))))
+
+
+@route('/fonts/<font_name:re:[a-zA-Z]+>.ttf')
+def baybayin_ttf(font_name):
+    filename = 'fonts/' + font_name + '.ttf'
+    return static_file(filename, root='static_files')
+
+
+run(host='localhost', port=8080, debug=True)
---
+++ static_files/fonts/README.txt
@@ -0,0 +1,4 @@
+Download and copy fonts into this directory.
+
+The font used by default is: BaybayinModernDoctrina.ttf
+(from http://nordenx.blogspot.com/2016/01/the-old-and-new.html)
